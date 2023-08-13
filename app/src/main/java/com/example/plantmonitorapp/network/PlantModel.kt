aad44e52ab258570c150819b6b09e47f71c68a9d
Torben Kneesch <Kneesch@gmx.de>
2023-08-13 12:27:16 +0200

    Add HTTP logging
    Remove counter-productive case-correction for JSON deserializer

--- app/src/main/java/com/example/plantmonitorapp/network/PlantModel.kt
+++ app/src/main/java/com/example/plantmonitorapp/network/PlantModel.kt
@@ -3,11 +3,11 @@ package com.example.plantmonitorapp.network
 import com.squareup.moshi.Json
 
 data class PlantModel(
-    @Json(name = "Id") val id: String,
-    @Json(name = "Name") val name: String,
-    @Json(name = "Description") val description: String,
-    @Json(name = "ImageUrl") val imageUrl: String,
-    @Json(name = "SensorId") val sensorId: Int? = null,
-    @Json(name = "AlertLevel") val alertLevel: Int = 0,
-    @Json(name = "IsAlertEnabled") val isAlertEnabled: Boolean = false
+    val id: String,
+    val name: String,
+    val description: String,
+    val imageUrl: String,
+    val sensorId: Int? = null,
+    val alertLevel: Int = 0,
+    val isAlertEnabled: Boolean = false
 )
--- app/src/main/java/com/example/plantmonitorapp/network/PlantWatchApiService.kt
+++ app/src/main/java/com/example/plantmonitorapp/network/PlantWatchApiService.kt
@@ -2,20 +2,30 @@ package com.example.plantmonitorapp.network
 
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 
 private const val BASE_URL =
-    "https://plantwatchers.onmicrosoft.com/ba03e8a4-d543-4d36-9d55-b639eadcb940/"
+    "https://plantmonitorwebappserver20220208190443.azurewebsites.net/"
 
 private val moshi = Moshi.Builder()
     .add(KotlinJsonAdapterFactory())
     .build()
 
+private val logging = HttpLoggingInterceptor().apply {
+    level = HttpLoggingInterceptor.Level.BODY
+}
+
+private val client = okhttp3.OkHttpClient.Builder()
+    .addInterceptor(logging)
+    .build()
+
 private val retrofit = Retrofit.Builder()
     .addConverterFactory(MoshiConverterFactory.create(moshi))
     .baseUrl(BASE_URL)
+    .client(client)
     .build()
 
 interface PlantWatchApiService {
